fc49a277e29fcdfb1893bc52f73c6656a8c45f5d
Vasilii Khomutov <vasilii@gainy.app>
2021-12-15 19:07:29 +0300

    * Update scripts to install `gainy_compute`

--- docker/meltano/init.sh
+++ docker/meltano/init.sh
@@ -3,6 +3,16 @@
 python scripts/generate_meltano_config.py $ENV
 ( cd scripts && python3 generate_rules_sql.py )
 
+# INSTALL `gainy-compute`
+GAINY_COMPUTE_VERSION=0.1.3
+
+CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain gainy-app --query authorizationToken --output text`
+CODEARTIFACT_REPOSITORY_URL=https://aws:$CODEARTIFACT_AUTH_TOKEN@gainy-app-217303665077.d.codeartifact.us-east-1.amazonaws.com/pypi/gainy-app/simple
+
+pip config set global.index-url $CODEARTIFACT_REPOSITORY_URL
+pip install gainy-compute==$GAINY_COMPUTE_VERSION
+
+
 while ! PGPASSWORD=$PG_PASSWORD pg_isready -h $PG_ADDRESS -p $PG_PORT -U $PG_USERNAME; do sleep 1; done
 echo 'Importing seeds'
 find seed -iname '*.sql' | sort | while read -r i; do
