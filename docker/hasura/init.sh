c60fed87ffb9cdea72787ed1f1a17849e47918fd
Mikhail Astashkevich <asmisha@tut.by>
2022-01-21 14:03:25 +0300

    try hasura default port for migration

--- docker/hasura/init.sh
+++ docker/hasura/init.sh
@@ -7,8 +7,8 @@ sed -i "s/schema: public\w*$/schema: $HASURA_GRAPHQL_PUBLIC_SCHEMA_NAME/" metada
 ### TODO:deployment_v2:versioned hasura views lambdas blocked by versioned lambdas
 
 # starting tmp server for migrations and metadata apply
-export HASURA_GRAPHQL_SERVER_PORT=8081
-export HASURA_GRAPHQL_ENDPOINT=http://localhost:$HASURA_GRAPHQL_SERVER_PORT
+#export HASURA_GRAPHQL_SERVER_PORT=8081
+#export HASURA_GRAPHQL_ENDPOINT=http://localhost:$HASURA_GRAPHQL_SERVER_PORT
 LOCKFILE=/run/graphql-engine.pid
 ( nohup graphql-engine serve 2>&1 & echo $! > $LOCKFILE ) > /proc/1/fd/1 &
 sleep 5
@@ -45,6 +45,6 @@ fi
 
 kill $(cat $LOCKFILE)
 
-export HASURA_GRAPHQL_SERVER_PORT=8080
-export HASURA_GRAPHQL_ENDPOINT=http://localhost:$HASURA_GRAPHQL_SERVER_PORT
+#export HASURA_GRAPHQL_SERVER_PORT=8080
+#export HASURA_GRAPHQL_ENDPOINT=http://localhost:$HASURA_GRAPHQL_SERVER_PORT
 graphql-engine serve
