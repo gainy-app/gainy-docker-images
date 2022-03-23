42044fe9bc95674a00bfee00f57436145089e3b0
Mikhail Astashkevich <asmisha@tut.by>
2022-03-23 18:16:56 +0400

    add function for continuous ms calculation

--- docker/meltano/init.sh
+++ docker/meltano/init.sh
@@ -14,6 +14,15 @@ PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -p $PG_PORT -U $PG_USERNAME $PG_DBNAME 
 
 PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -p $PG_PORT -U $PG_USERNAME $PG_DBNAME -c "CREATE SCHEMA IF NOT EXISTS $DBT_TARGET_SCHEMA;"
 
+# Function: CDF of Normal Gaussian Distribution (mu=0,sigma=1, (so prestandardize input))
+PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -p $PG_PORT -U $PG_USERNAME $PG_DBNAME -c "CREATE OR REPLACE FUNCTION $DBT_TARGET_SCHEMA.pnorm(z double precision) RETURNS double precision AS \$\$
+                                                                                    SELECT CASE -- calculation by erf-function expansion to series (error= +/-2e-7, const 0 if <-7 AND 1 if >7)
+                                                                                               when \$1 <-1e4 then 0.0 when \$1 >1e4 then 1.0
+                                                                                               when \$1 >= 0 then 1 - POWER(((((((0.000005383*\$1+0.0000488906)*\$1+0.0000380036)*\$1+0.0032776263)*\$1+0.0211410061)*\$1+0.049867347)*\$1+1),-16)/2
+                                                                                               else 1 - $DBT_TARGET_SCHEMA.pnorm(-\$1)
+                                                                                               END;
+                                                                                    \$\$ LANGUAGE SQL IMMUTABLE STRICT;"
+
 if ! PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -p $PG_PORT -U $PG_USERNAME $PG_DBNAME -c "select count(*) from $DBT_TARGET_SCHEMA.deployment_metadata"; then
   echo 'Running csv-to-postgres' && meltano schedule run csv-to-postgres --force
 else
