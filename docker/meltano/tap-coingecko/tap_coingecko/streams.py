72188bf21a07467990d6f18091eb660b749f06bc
Mikhail Astashkevich <asmisha@tut.by>
2022-04-05 10:35:19 +0400

    add backoff on 429 error

--- docker/meltano/tap-coingecko/tap_coingecko/streams.py
+++ docker/meltano/tap-coingecko/tap_coingecko/streams.py
@@ -1,52 +1,16 @@
 """Stream type classes for tap-coingecko."""
-from abc import ABC
-from datetime import datetime
-from datetime import timedelta
-from functools import cached_property, reduce
+from functools import cached_property
 from pathlib import Path
-from typing import Any, Dict, Optional, Iterable, List
+from typing import Any, Dict, Optional, List
 
 import requests
-import singer
-from singer import RecordMessage
-from singer_sdk.helpers._typing import conform_record_data_types
-from singer_sdk.helpers._util import utc_now
 
 from tap_coingecko.client import CoingeckoStream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
-class AbstractCoingeckoStream(CoingeckoStream):
-    def _write_record_message(self, record: dict) -> None:
-        """Write out a RECORD message."""
-        record = conform_record_data_types(
-            stream_name=self.name,
-            row=record,
-            schema=self.schema,
-            logger=self.logger,
-        )
-        for stream_map in self.stream_maps:
-            mapped_record = stream_map.transform(record)
-            # Emit record if not filtered
-            if mapped_record is not None:
-                record_message = RecordMessage(
-                    stream=stream_map.stream_alias,
-                    record=mapped_record,
-                    version=None,
-                    time_extracted=utc_now(),
-                )
-                singer.write_message(record_message)
-
-    def get_records(self, context: Optional[dict]) -> Iterable[Dict[str, Any]]:
-        try:
-            yield from super().get_records(context)
-        except Exception as e:
-            self.logger.error('Error while requesting %s for coin %s: %s' % (self.name, context['id'], str(e)))
-            pass
-
-
-class CoinData(AbstractCoingeckoStream):
+class CoinData(CoingeckoStream):
     name = "coingecko_coin"
     path = "/v3/coins/{id}"
     primary_keys = ["id"]
