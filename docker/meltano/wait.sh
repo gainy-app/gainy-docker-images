2784b53a1038e108da24608c035a042817ef882e
Mikhail Astashkevich <asmisha@tut.by>
2022-04-29 18:39:41 +0400

    fix

--- docker/meltano/wait.sh
+++ docker/meltano/wait.sh
@@ -4,6 +4,8 @@ for i in /usr/lib/postgresql/*/bin; do
   export PATH=$PATH:$i
 done
 
+python scripts/generate_meltano_config.py
+
 while ! PGPASSWORD=$PG_PASSWORD pg_isready -h $PG_HOST -p $PG_PORT -U $PG_USERNAME; do sleep 1; done &> /dev/null
 while ! PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -p $PG_PORT -U $PG_USERNAME $PG_DBNAME -c "select * from meltano.alembic_version"; do sleep 1; done &> /dev/null
 
