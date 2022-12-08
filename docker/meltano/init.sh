9ea5e7fecc72df7c3d152f1f845ac56ee8d12206
Mikhail Astashkevich <asmisha@tut.by>
2022-12-08 08:07:51 +0100

    fix

--- docker/meltano/init.sh
+++ docker/meltano/init.sh
@@ -6,6 +6,8 @@ done
 
 while ! PGPASSWORD=$PG_PASSWORD pg_isready -h $PG_HOST -p $PG_PORT -U $PG_USERNAME; do sleep 1; done
 
+unset MELTANO_STATE_BACKEND_S3_AWS_ACCESS_KEY_ID
+
 find /init.d -maxdepth 1 -type f | sort | while read -r i; do
   chmod +x $i
   set -a
